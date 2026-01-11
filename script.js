aba32c9b195e1da122a3b3f99ad02523e123a243
lazaroduboy4-corp <lazaroduboy4@gmail.com>
2026-01-11 14:03:36 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleService(service) {
   const content = service.querySelector(".service-content");
   content.style.display = content.style.display === "block" ? "none" : "block";
-} 
+}
