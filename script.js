846d8f8c8f078e3eada4d0cf0e864970ac8a3372
lazaroduboy4-corp <lazaroduboy4@gmail.com>
2026-01-10 20:53:10 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-const telefonoWhatsApp = "17542365536";
-
-document.querySelectorAll("form").forEach(form => {
-    form.addEventListener("submit", e => {
-        e.preventDefault();
-
-        let mensaje = "Hola, soy cliente de Duboy Services Corp.%0A%0A";
-        const inputs = form.querySelectorAll("input, textarea");
-
-        inputs.forEach(input => {
-            if (input.value && input.type !== "hidden") {
-                mensaje += `${input.placeholder || input.name}: ${input.value}%0A`;
-            }
-        });
-
-        window.open(`https://wa.me/${telefonoWhatsApp}?text=${mensaje}`, "_blank");
-        form.submit();
-    });
-});
+function toggleService(service) {
+  const content = service.querySelector(".service-content");
+  content.style.display = content.style.display === "block" ? "none" : "block";
+}
